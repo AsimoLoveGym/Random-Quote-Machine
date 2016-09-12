5f74d76d317d799272e26ae24ea7f109697a5a66
AsimoLoveGym <xiazhuangz@gmail.com>
2016-09-12 08:01:09 -0500

    feat: finish the twitter link to tweet out
    extra TODO: add another API to load author's pic

--- js/main.js
+++ js/main.js
@@ -1,12 +1,23 @@
+var quote = "";
+var author = "Anonymous Author";
+
 $(document).ready(function(){
   $("#requestQuote").on("click",function(){
     $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?",function(json){
-        $("#quote").html(json.quoteText);
+        quote = json.quoteText;
+        $("#quote").html(quote);
         if (json.quoteAuthor) {
-          $("#author").html("-  " + json.quoteAuthor);
+          author = json.quoteAuthor;
+          $("#author").html("  -- " + json.quoteAuthor);
         } else {
-          $("#author").html("-  Anonymous Author");
+          $("#author").html("  -- Anonymous Author");
         }
     });
   });
 });
+
+function twitterOut() {
+  quote = $("#quote").html();
+  author = $("#author").html();
+  window.open("https://twitter.com/intent/tweet?text=" + quote + author);
+}
